8bea5908d96c0f45bba24507fdedd0e52b23d2d0
nezmustafa123 <nezmustafa123@gmail.com>
2020-03-06 19:11:49 +0000

    added jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,3 +21,21 @@ $('.client-carousel').owlCarousel({
 });
 
 
+$(document).ready(function(){
+	
+	$('h1').click(function(){
+		$(this).css('background-color', '#ff0000');
+	})
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
