1eb4f0c52ffedaa8d7602565afa0311ee351ece6
nezmustafa123 <nezmustafa123@gmail.com>
2020-03-07 16:18:32 +0000

    added waypoints

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,21 +14,23 @@ $('.client-carousel').owlCarousel({
 			items: 5
 		}
 	}
-	
-	
-	
-	
 });
 
+$('.js--section-features').waypoint(function(direction){
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+			
+		} else {
+			$('nav').removeClass('sticky');
+		}
+		}, {
+			offset: '15%;'
+	});
 
-$(document).ready(function(){
-	
-	$('h1').click(function(){
-		$(this).css('background-color', '#ff0000');
-	})
 	
 	
-});
+	
+
 
 
 
