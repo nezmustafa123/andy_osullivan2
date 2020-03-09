ca50c6b17e27b8e39a9453f03d83025c693e8194
nezmustafa123 <nezmustafa123@gmail.com>
2020-03-09 21:13:51 +0000

    media query nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -69,7 +69,16 @@ $('.js--section-features').waypoint(function(direction){
 
 
 
+const iconMenu = document.querySelector('.menu_toggler');
+const menu = document.querySelector('.menu');
 
+
+
+
+iconMenu.addEventListener('click', function() {
+    this.classList.toggle('active');
+    menu.classList.toggle('active');
+})
 
 
 
