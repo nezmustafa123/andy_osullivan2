ec191413e386bf3e74e8323228075c1ecc77d791
nezmustafa123 <nezmustafa123@gmail.com>
2020-05-16 20:50:48 +0100

    edited media queries for screen sizes 1023px

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     
-//$('.client-carousel').owlCarousel({
-//        loop: true,
-//        margin: 10,
-//        autoplay: true,
-//        autoplaySpeed: 1000,
-//        responsive: {
-//            0: {
-//                items: 1
-//            },
-//            600: {
-//                items: 3
-//            },
-//            1000: {
-//                items: 5
-//            }
-//        }
-//    });
+$('.client-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
 
     $('.js--section-features').waypoint(function (direction) {
         if (direction === "down") {
